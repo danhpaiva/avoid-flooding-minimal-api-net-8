808b75a3bb799adffe6b906ae45c318076b9663d
Paiva <danpaiva@live.com>
2024-04-16 14:34:18 -0300

    feat: add dbcontext

---
+++ AvoidFloodingApi/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AvoidFloodingApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvoidFloodingApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Localizacao>? Localizacoes { get; set; }
+    }
+}
