917faad783c89f1518195a59c2aba9e294066d07
Paiva <danpaiva@live.com>
2024-04-16 14:47:21 -0300

    refact: modificando dados da DbContext

--- AvoidFloodingApi/Context/AppDbContext.cs
+++ AvoidFloodingApi/Context/AppDbContext.cs
@@ -8,5 +8,26 @@ namespace AvoidFloodingApi.Context
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Localizacao>? Localizacoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Localizacao>().HasKey(c => c.LocalizacaoId);
+            mb.Entity<Localizacao>().Property(c => c.Nome)
+                                    .HasMaxLength(200)
+                                    .IsRequired();
+            mb.Entity<Localizacao>().Property(c => c.Latitude)
+                                   .HasMaxLength(15)
+                                   .IsRequired();
+            mb.Entity<Localizacao>().Property(c => c.Longitude)
+                                   .HasMaxLength(15)
+                                   .IsRequired();
+            mb.Entity<Localizacao>().Property(c => c.ProbabilidadeAlagamento)
+                                   .HasPrecision(3,2)
+                                   .HasMaxLength(15)
+                                   .IsRequired();
+            mb.Entity<Localizacao>().Property(c => c.DataAtualizacao)
+                                  .HasMaxLength(15)
+                                  .IsRequired();
+        }
     }
 }
